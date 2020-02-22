d299b614d991200cab484d4acaf8d1872f3780b3
Ching Lam Choi <ccl5a09@gmail.com>
2020-02-23 00:26:14 +0800

    Add files via upload

---
+++ loss.py
@@ -0,0 +1,19 @@
+import torch.nn as nn
+
+
+class DiceLoss(nn.Module):
+
+    def __init__(self):
+        super(DiceLoss, self).__init__()
+        self.smooth = 1.0
+
+    def forward(self, y_pred, y_true):
+        assert y_pred.size() == y_true.size()
+        y_pred = y_pred[:, 0].contiguous().view(-1)
+        y_true = y_true[:, 0].contiguous().view(-1)
+        intersection = abs(y_pred * y_true).sum()
+        #print(intersection)
+        dsc = (2. * intersection + self.smooth) / (
+            y_pred.sum() + y_true.sum() + self.smooth
+        )
+        return 1-dsc
