5a8b5084a1d604392c26eb7f221e23f8cd6c4e01
Ching Lam Choi <ccl5a09@gmail.com>
2020-03-22 16:31:58 +0800

    Update test.py

--- test.py
+++ test.py
@@ -7,7 +7,9 @@ from torchvision import transforms as transforms
 import numpy as np
 import os
 from skimage.segmentation import find_boundaries
+#from skimage.metrics import adapted_rand_error
 from sklearn.metrics import adjusted_rand_score
+#from loss import DiceLoss
 from skimage import io
 import unet_multi
 import unet
@@ -50,14 +52,26 @@ def DiceLoss(a,b):
 
 """
 def PixelLoss(a,b):
-    a = (a > 0.4).float()
+    a = (a > 0.33).float()
     c = find_boundaries(a.cpu().numpy(), background=0)
     d = find_boundaries(b.cpu().numpy(), background=0)
     return np.linalg.norm(np.subtract(c,d, dtype=np.float32)) #precompute for trainset
 """
+class UNet2(nn.Module):
+    def __init__(self):
+        super(UNet2, self).__init__()
+        self.outc1 = nn.AdaptiveAvgPool2d((1,1))
+        self.outc2 = nn.Linear(1,1)
+    def forward(self, x):
+        out = self.outc1(x)
+        out = self.outc2(out.view(out.size(0), -1))
+        return x, out
+
+def run_cnn2():
+    return UNet2()
 
 def RandLoss(a,b):
-    a = (a >= 0.5).float()
+    a = (a >= 0.35).float()
     a = a.reshape((256, 256))
     b = b.reshape((256,256))
     a = a.cpu().numpy().flatten()
@@ -66,25 +80,36 @@ def RandLoss(a,b):
     c = (c+1)/2
     return 1 - c
 
-#torch.multiprocessing.freeze_support()
+
+def hook_feature(module, input, output):
+    feature_blobs.append(output.cpu().data.numpy())
+
+def returnCAM(feature_conv, weight):
+    _, nc, h, w = feature_conv.shape
+    output_cam = []
+    for i in range(_):
+        cam = weight.dot(feature_conv[i].reshape((nc, h*w)))
+        cam = (torch.from_numpy(cam.reshape(h, w))).reshape(1, h, w)
+        output_cam.append(cam)
+    return torch.stack(output_cam)
+
 if __name__ == "__main__":
     test_label_file = np.load("./test_labels.npy")
     testset = BrainTumour(img_dir='./TCGA/imgs/test/', mask_dir='./TCGA/masks/test/', lbs=test_label_file)
     testloader = data.DataLoader(testset, batch_size=1, shuffle=False, num_workers=4)
     
-    #net = unet.run_cnn()
-    net = unet_multi.run_cnn()
-    pretrain = input("File path of pretrained model: ")
-    checkpoint = torch.load("report/" + pretrain + ".pt", map_location="cuda:0")
-    #checkpoint = torch.load("./TCGA/newest_adam/" + pretrain)
-    net.load_state_dict(checkpoint["net"])
     a = "cuda:0"
     device = torch.device(a if torch.cuda.is_available() else "cpu")
-    if device != "cpu":
-        net.to(device)
 
+    net = unet.run_cnn()
+    net2 = run_cnn2()
+
+    pretrain = input("File path of pretrained model: ")
+    checkpoint = torch.load("report/" + pretrain + ".pt", map_location="cuda:0")
+    net.load_state_dict(checkpoint["net"])
+
+    net.to(device)
     net = net.eval()
-
     path_ = "./TCGA/mask_pred/" + pretrain + "/"
     try:
         os.mkdir(path_)
@@ -92,21 +117,20 @@ if __name__ == "__main__":
         pass
 
     tot = 0.0
+    tot2 = 0.0
     tot_rand = 0.0
     #a = 0
     with torch.no_grad():
         tp, fp, tn, fn = 0,0,0,0
+        tp1, fp1, tn1, fn1 = 0,0,0,0
         for img, mask, lb, idx in testloader:
+            weights = np.squeeze(list(net.parameters())[-2].cpu().data.numpy())
+            feature_blobs = []
             img, mask = img.to(device), mask.to(device)
-            mask_pred, _ = net(img)
-            #mask_pred = net(img)
-            #t = _.item()
+            mask_pred = net(img)
             t = torch.sigmoid(mask_pred)
-            #_ = (torch.max(t)-torch.min(t)).item()
-            #a = _ if _ > a else a
-            #print(a, torch.max(mask_pred))
+
             if len(t[t >=0.5]) > 0:
-            #if t >= 0.5:
                 tp = tp + 1 if lb.item() >= 0.5 else tp
                 fp = fp + 1 if lb.item() < 0.5 else fp
             else:
@@ -120,10 +144,9 @@ if __name__ == "__main__":
             tot_rand[1] += _[1]
             tot_rand[2] += _[2]
             """
-            tot_rand += RandLoss(t, mask)
-            #a += 1
-            mask_pred.to("cpu")
-            torchvision.utils.save_image(mask_pred, path_ + idx[0])
+            tot_rand += RandLoss(t_, mask)
+            t_.to("cpu")
+            torchvision.utils.save_image(t_, path_ + idx[0])
         """
         print("ROC-AUC Score: ", roc_auc_score(y_true, y_scores))
         fpr, tpr, _ = roc_curve(y_true, y_scores)
@@ -151,6 +174,6 @@ if __name__ == "__main__":
         print("F1 score:", 2*precision*recall/(precision+recall))
     except:
         print("F1 score:", 0)
-    print("Dice Loss:", tot.item()/759) #dice loss
-    print("Rand Score:", tot_rand/759)
+    print("Dice Loss:", tot/759) #dice loss
+    print("Rand Lcore:", tot_rand/759)
     #print("Rand error: %f | Rand precision: %f | Rand recall: %f "%(tot_rand[0]/759, tot_rand[1]/759, tot_rand[2]/759))
